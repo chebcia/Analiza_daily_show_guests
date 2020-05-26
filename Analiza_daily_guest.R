4e68be4bf62ceb6b91bafaeacc56f3fb66c6e410
Natalia Cheba <43813992+chebcia@users.noreply.github.com>
2020-05-26 10:12:25 +0200

    Update Analiza_daily_guest.R

--- Analiza_daily_guest.R
+++ Analiza_daily_guest.R
@@ -5,4 +5,10 @@ guests <- separate(guests, col = c("raw_guest_list"), into = c("imie", "nazwisko
 
 #zmiana na 3 kolumny - rozdzielenie daty
 guests<-separate(guests, col = c("show"), into = c("year", "month", "day"), sep = "-", extra = "merge")
- 
+
+guests$year <-as.numeric(guests$year)
+guests$month <-as.numeric(guests$month)
+guests$day <-as.numeric(guests$day)
+
+
+guests$group <-as.factor(guests$group)
